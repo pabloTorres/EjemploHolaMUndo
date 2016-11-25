0537865766fc4a982cd042886994299ae12c399e
pabloTorres <pabloblo2d@hotmail.com>
2016-11-25 12:36:16 -0500

    PrimeraVersión

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola mundo 1ra versión");
+
+ 
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
